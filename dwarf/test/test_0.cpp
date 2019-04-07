c91ac914bc4c70e9795aee28ca1c3018d2d87f33
Laurent Stacul <laurent.stacul@gmail.com>
2019-04-07 21:28:56 +0200

    Add small tests for DWARF

---
+++ dwarf/test/test_0.cpp
@@ -0,0 +1,4 @@
+unsigned int add(unsigned int a, unsigned int b)
+{
+    return a+b;
+}
---
+++ dwarf/test/test_1.cpp
@@ -0,0 +1,7 @@
+namespace stac {
+typedef unsigned int integer_t;
+void function(integer_t iInput)
+{
+    return;
+}
+} /* namespace stac */
